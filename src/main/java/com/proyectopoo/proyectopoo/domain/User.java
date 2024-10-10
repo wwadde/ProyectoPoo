3f862596aecee379e351bb3a32f3dc45eb189473
jhoan.montealegre <jhoan.montealegre@linktic.com>
2024-10-09 20:36:40 -0500

    Adicion entidad usuario

---
+++ src/main/java/com/proyectopoo/proyectopoo/domain/User.java
@@ -0,0 +1,12 @@
+package com.proyectopoo.proyectopoo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private Long id;
+}
